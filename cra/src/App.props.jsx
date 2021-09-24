8d600d37eea2c0ec54d426fc041822747d4e87ea
박일규 <t5nn123@naver.com>
2021-09-24 09:42:48 +0900

    Props로 Children 전달

---
+++ cra/src/App.props.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import PropClass from "./components/PropClass";
+import PropFunc from "./components/PropFunc";
+
+function App() {
+  const messageFromChild = (message) => {
+    alert(message);
+  };
+
+  return (
+    <div>
+      <h2>Props in Class Component</h2>
+      <PropClass
+        prop_str="string value"
+        prop_num={10}
+        prop_false={false}
+        prop_true
+        prop_obj={{
+          name: "홍길동",
+          age: 28,
+        }}
+        prop_func={messageFromChild}
+      >
+        <h4>Children</h4>
+        <p>자식 컴포넌트</p>
+      </PropClass>
+      <h2>Props in Function Component</h2>
+      <PropFunc
+        prop_str="string value"
+        prop_num={10}
+        prop_false={false}
+        prop_true
+        prop_obj={{
+          name: "홍길동",
+          age: 28,
+        }}
+        prop_func={messageFromChild}
+      />
+    </div>
+  );
+}
+
+export default App;
